f2a09b4b33e98768e847dbd40b53605e60dee323
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-09-28 13:26:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 5000,
-    strictPort: true,
-    allowedHosts: ['.lovable.dev']
+    allowedHosts: 'all'
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
