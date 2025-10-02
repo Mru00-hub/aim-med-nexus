063e8bc57a7a865f88e56e47e3b21de5fb9d616e
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-02 09:34:45 +0530

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -713,8 +713,9 @@ const Register = () => {
                                   <SelectValue placeholder="Years of experience" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                  <SelectItem value="0-1">0-1 years</SelectItem>
-                                  <SelectItem value="1-5">1-5 years</SelectItem>
+                                  <SelectItem value="fresh">0-1 years</SelectItem>
+                                  <SelectItem value="1-3">1-3 years</SelectItem>
+                                  <SelectItem value="3-5">3-5 years</SelectItem>
                                   <SelectItem value="5-10">5-10 years</SelectItem>
                                   <SelectItem value="10+">10+ years</SelectItem>
                                 </SelectContent>
