109f29478d75eac1196e01bf99c06d134531e22d
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-03 13:29:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,8 @@ const App = () => (
             <Route path="/register" element={<AuthGuard requireAuth={false}><Register /></AuthGuard>} />
             <Route path="/please-verify" element={<PleaseVerify />} />
             <Route path="/login" element={<AuthGuard requireAuth={false}><Login /></AuthGuard>} />
+            <Route path="/auth/callback" element={<AuthGuard><AuthCallback /></AuthGuard>} />
+            <Route path="/complete-profile" element={<AuthGuard><CompleteProfile /></AuthGuard>}
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
             
