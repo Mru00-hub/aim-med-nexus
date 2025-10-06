517a18783a987514b7f36a5c1f9999cc4a2e30cf
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-06 22:13:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+if (import.meta.env.DEV) {
+  import('eruda').then(eruda => eruda.default.init());
+}
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
