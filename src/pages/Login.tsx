8b3ad207e86b3284f46f68cb3aabde439ca5b257
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-12 13:48:41 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -143,16 +143,6 @@ const Login = () => {
                 </Button>
               </form>
 
-              {/* Divider */}
-              <div className="relative my-6">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-border"></div>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-                </div>
-              </div>
-
               {/* Sign Up Link */}
               <div className="text-center mt-6">
                 <p className="text-muted-foreground">
