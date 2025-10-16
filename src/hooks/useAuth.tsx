299d9ee596f7c49082dc1ca5b7e799d539263349
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-16 11:39:59 +0530

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -221,6 +221,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     loading,
     loadingMessage,
     encryptionKey,
+    generateAndSetKey,
     refreshProfile,
     signUp,
     signIn,
