5c54716bb5f339c07e0caed32da8809e9f0ed896
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:12:13 +0530

    Create Report.tsx

---
+++ src/pages/Info/Report.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function ReportPage() {
+  return (
+    <InfoPageLayout title="Report Content">
+      <p>If you see content that violates our policies or code of conduct, please let us know.</p>
+      <p>Email us at: <a href="mailto:mrudulabhalke75917@gmail.com">mrudulabhalke75917@gmail.com</a> with a link to the content and a brief description of the issue.</p>
+    </InfoPageLayout>
+  );
+}
