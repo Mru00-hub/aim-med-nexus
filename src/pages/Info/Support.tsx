83e8289320a13196e9246f378f383da1ca289d5f
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:11:42 +0530

    Create Support.tsx

---
+++ src/pages/Info/Support.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function SupportPage() {
+  return (
+    <InfoPageLayout title="Contact Support">
+      <p>If you need help, please reach out to our support team at:</p>
+      <a href="mailto:mrudulabhalke75917@gmail.com">mrudulabhalke75917@gmail.com</a>
+    </InfoPageLayout>
+  );
+}
