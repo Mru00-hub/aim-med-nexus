590a8b75ed8fbfcc5aeff0587153092adad72f53
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:13:31 +0530

    Create Terms.tsx

---
+++ src/pages/Info/Terms.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function TermsPage() {
+  return (
+    <InfoPageLayout title="Terms of Service">
+      <p>Welcome to AIMedNet. By using our services, you agree to these terms.</p>
+      {/* Add your full terms of service text here */}
+    </InfoPageLayout>
+  );
+}
