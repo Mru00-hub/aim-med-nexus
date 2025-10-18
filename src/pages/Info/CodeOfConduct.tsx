5295aa7a8610ecbc9238743c9e44b98d79df753d
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:14:59 +0530

    Create CodeOfConduct.tsx

---
+++ src/pages/Info/CodeOfConduct.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function CodeOfConductPage() {
+  return (
+    <InfoPageLayout title="Code of Conduct">
+      <p>AIMedNet is dedicated to providing a professional and respectful environment for all members.</p>
+      {/* Add your full code of conduct text here */}
+    </InfoPageLayout>
+  );
+}
