439a6e25a7abbab24f514e7b21b8bc890ca3a924
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:13:05 +0530

    Create Privacy.tsx

---
+++ src/pages/Info/Privacy.tsx
@@ -0,0 +1,11 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function PrivacyPage() {
+  return (
+    <InfoPageLayout title="Privacy Policy">
+      <p>Your privacy is important to us. This is where your privacy policy will go.</p>
+      <p>Last updated: [Date]</p>
+      {/* Add your full privacy policy text here */}
+    </InfoPageLayout>
+  );
+}
