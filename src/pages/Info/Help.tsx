a0457ca1e62b3ae0cfc51e4e858150206a7a006a
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:09:47 +0530

    Create Help.tsx

---
+++ src/pages/Info/Help.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function HelpPage() {
+  return (
+    <InfoPageLayout title="Help Center">
+      <p>Welcome to the AIMedNet Help Center. Please browse our FAQs or contact support for assistance.</p>
+      {/* Add FAQs or help content here */}
+    </InfoPageLayout>
+  );
+}
