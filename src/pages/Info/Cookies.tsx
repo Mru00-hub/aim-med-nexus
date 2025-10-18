e90c95094261f17fe37844fe31b2c4201c941924
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-18 13:14:03 +0530

    Create Cookies.tsx

---
+++ src/pages/Info/Cookies.tsx
@@ -0,0 +1,10 @@
+import { InfoPageLayout } from './InfoPageLayout';
+
+export default function CookiesPage() {
+  return (
+    <InfoPageLayout title="Cookie Policy">
+      <p>This is where your cookie policy will go, explaining how you use cookies on AIMedNet.</p>
+      {/* Add your full cookie policy text here */}
+    </InfoPageLayout>
+  );
+}
