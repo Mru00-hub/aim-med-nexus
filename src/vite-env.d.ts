491cb270cb3731e5a9a55a7d547e8d8762c418f0
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-20 10:39:48 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string
+  readonly VITE_SYSTEM_ACTOR_ID: string // <-- ADD THIS
+  readonly VITE_ADMIN_USER_ID: string   // <-- AND ADD THIS
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
