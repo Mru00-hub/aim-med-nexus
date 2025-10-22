90d54b5a53c9b7021c6c45135d2e6ab96efb214e
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-22 18:59:20 +0530

    Update SpaceDetailPage.tsx

--- src/pages/Community/SpaceDetailPage.tsx
+++ src/pages/Community/SpaceDetailPage.tsx
@@ -320,6 +320,7 @@ export default function SpaceDetailPage() {
                     </AlertDialog>
                   </div>
                 )}
+              </div>
               </CardContent>
             </Card>
           );
