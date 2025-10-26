b71da3aa1fe6bc2088deb5f9fa574491ef0c0b56
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-10-26 18:53:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { SocialCountsProvider } from './context/SocialCountsContext';
 import { SecureRouteGuard } from './components/SecureRouteGuard';
 import SettingsPage from './pages/SettingsPage';
 import { Loader2 } from 'lucide-react';
+import './App.css';
 
 // --- All Page Imports (remain the same) ---
 import Index from "./pages/Index";
