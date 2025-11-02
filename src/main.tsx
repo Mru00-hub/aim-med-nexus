50e3e024f99faa18a3c554769faa7300ebf2b187
Mru00-hub <mrudulabhalke75225@gmail.com>
2025-11-02 17:53:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+console.log('workerSrc (global setup):', pdfjs.GlobalWorkerOptions.workerSrc);
 if (import.meta.env.DEV) {
   import('eruda').then(eruda => eruda.default.init());
 }
